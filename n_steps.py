634a4804e513b27f698571ed5583786010af40cb
dmitriygorbatovskii <70207059+dmitriygorbatovskii@users.noreply.github.com>
2020-12-31 23:26:13 +0300

    Update n_steps.py

--- n_steps.py
+++ n_steps.py
@@ -11,7 +11,7 @@ for y in range(-9, 10):
 env = env_2.test_env()
 a = []
 b = []
-alpha = 0.01
+alpha = 0.001
 for i in range(5000):
     observation = env.reset()
     v = 0
@@ -26,6 +26,7 @@ for i in range(5000):
         obs2, reward1, done, info = env.step(action2)
         result3 = [item for item in Q if item[:2] == obs2]
         action3 = list(result3)[0][2:].index(max(list(result3)[0][2:]))
+        obs3, reward2, done, info = env.step(action3)
         list(result1)[0][action+2] = list(result1)[0][action+2]+ alpha * list(result2)[0][action2+2]\
                                      + alpha * (reward + 1 * (list(result3)[0][action3+2]) - list(result1)[0][action+2])
         observation = obs
@@ -35,10 +36,11 @@ for i in range(5000):
         if done:
             list(result2)[0][action2 + 2] = list(result2)[0][action2 + 2] + alpha * (
                         reward1 + 1 * (list(result2)[0][action3 + 2]) - list(result2)[0][action2 + 2])
-            list(result3)[0][action3 + 2] = 0.01 * reward
-
-            print(i, v-min_steps)
-            a.append(v-min_steps)
+            list(result3)[0][action3 + 2] = alpha * reward2
+            '''list(result2)[0][action2 + 2] += alpha * reward1
+            list(result3)[0][action3 + 2] += alpha * reward2'''
+            print(i, v+2-min_steps)
+            a.append(v+2-min_steps)
             b.append(i)
             break
 
